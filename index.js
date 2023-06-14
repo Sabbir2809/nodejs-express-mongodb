54d65dea08a6a85c4653c5dd76d6f7160e12b432
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-06-14 19:55:51 +0600

    Chapter-5: Backend Directory Structure / MVC / Router

--- index.js
+++ index.js
@@ -1,63 +1,13 @@
-// Dependencies
 const express = require('express');
 const server = express();
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
 
-//Middleware
+//bodyParser
 server.use(express.json());
 server.use(express.static('public'));
-
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('./public/data.json', 'utf-8'));
-const products = data.products;
-
-// API ROOT , base URL, example - google.com/api/v2/
-// API - Endpoint - Route
-// C R U D
-
-//Create POST /products
-server.post('/products', (req, res) => {
-  console.log(req.body);
-  products.push(req.body);
-  res.status(201).json(req.body);
-});
-
-// Read GET /products
-server.get('/products', (req, res) => {
-  res.json(products);
-});
-
-// Read GET /products/:id
-server.get('/products/:id', (req, res) => {
-  const id = +req.params.id;
-  const product = products.find((p) => p.id === id);
-  res.json(product);
-});
-
-// Update PUT /products/:id
-server.put('/products/:id', (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  products.splice(productIndex, 1, { ...req.body, id: id });
-  res.status(201).json();
-});
-
-// Update PATCH /products/:id
-server.patch('/products/:id', (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1, { ...product, ...req.body });
-  res.status(201).json();
-});
-
-// Delete DELETE /products/:id
-server.delete('/products/:id', (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1);
-  res.status(201).json(product);
-});
+server.use('/products', productRouter.router);
+server.use('/users', userRouter.router);
 
 server.listen(8080, () => {
   console.log('server started');
